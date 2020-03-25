3205805534c44931db573072373b1b8227cf2e0a
Thiti Dev <thiti.mwk.main@gmail.com>
2020-03-26 02:32:34 +0700

    :white_check_mark: Dummy testing with redux-hook

--- client/src/components/pages/Dashboard/Dashboard.jsx
+++ client/src/components/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Dashboard() {
+	const _dState = useSelector((state) => state.dashboard);
+	console.log(_dState);
 	return (
 		<div>
 			<h1>This is the dashboard</h1>
+			<p>{_dState.test}</p>
 		</div>
 	);
 }
